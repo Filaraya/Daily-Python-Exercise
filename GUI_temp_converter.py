8c64261af342d6d19919f146c109c0afe72a1771
Filmon Araya <40590856+Filaraya@users.noreply.github.com>
2021-01-10 21:52:01 -0800

    Create GUI_temp_converter.py

---
+++ GUI_temp_converter.py
@@ -0,0 +1,27 @@
+"""
+writer a program using GUI program that converts temperatures from Fahrenheit to Celsius.
+"""
+from tkinter import *
+
+root = Tk()
+mainloop ()
+
+def calculate ():
+    temp = int (entry.get ())
+    temp = 9/5 * temp + 32
+    output_label.configure(text = 'The temprature in Fahrenheit: {:.1f}'.format(temp))
+    entry.delete(0,END)
+root = Tk()
+message_label = Label(text='Enter a temprature in Celcius',
+font=('Verdana', 16))
+output_label = Label(font=('Verdana', 16))
+entry = Entry(font=('Verdana', 16), width=4)
+calc_button = Button(text='Ok', font=('Verdana', 16),
+command=calculate)
+message_label.grid(row=0, column=0)
+entry.grid(row=0, column=1)
+calc_button.grid(row=0, column=2)
+output_label.grid(row=1, column=0, columnspan=3)
+    
+    
+mainloop()
