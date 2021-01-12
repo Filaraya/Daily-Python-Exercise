49eb8ace92a905292ed2221a48c3a29b13bd56a8
Filmon Araya <40590856+Filaraya@users.noreply.github.com>
2021-01-12 15:51:42 -0800

    updated

---
+++ even_or_odd.py
@@ -0,0 +1,1 @@
+"""Ask the user for a number.Depending on whether the number is even or odd,print out an appropriate message to the user.Hint: how does an even / odd number react differently when divided by 2?"""enter_number = int(input("Please enter any integer: "))result= enter_number % 2if result == 0:    print("the number is Even")else:    print("the number is Odd")    
