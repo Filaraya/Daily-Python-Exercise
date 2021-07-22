2b8cd36e5112b7358c61e80dcdd76ee03e0a351c
filaraya <filijosi@gmail.com>
2021-07-21 22:25:06 -0700

    Update IF Statements.py

--- IF Statements.py
+++ IF Statements.py
@@ -17,4 +17,12 @@ if your_city not in cities:
     print("your city is not in the list")
 else:
     print("your city is in the list")
-    
+#Testing Multiple Conditions
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+print("\nFinished making your pizza!")
