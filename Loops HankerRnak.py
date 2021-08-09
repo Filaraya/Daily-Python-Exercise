9d9b925842924086a2c547b64fea18f4ef9b3477
filaraya <filijosi@gmail.com>
2021-08-08 22:44:29 -0700

    file added

---
+++ Loops HankerRnak.py
@@ -0,0 +1,7 @@
+"""
+The provided code stub reads and integer, , from STDIN.
+For all non-negative integers i<n, print i**2 .
+"""
+n = int(input("Enter a number: "))
+for i in range(0,n):
+    print(i**2)
