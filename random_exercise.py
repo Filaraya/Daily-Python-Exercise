2a42a59e3444e8aa8c77e13d149e2f4b061329a0
filaraya <filijosi@gmail.com>
2021-08-26 22:27:41 -0700

    Update random_exercise.py

--- random_exercise.py
+++ random_exercise.py
@@ -1,4 +1,4 @@
 from random import choice
 names = ['David', 'Ahmed', 'Smith','Susan']
 current_user = choice(names)
-print (f"selecte any current user: {current_user}")
+print (f"selecte any current user from the list: {current_user}")
