12e47e046a6eb6c36488c9fc6fa618c8ce11ca07
Filmon Y Araya <40590856+Filaraya@users.noreply.github.com>
2021-11-19 21:57:49 -0800

    Update Movie Ticket using while loop.py

--- Movie Ticket using while loop.py
+++ Movie Ticket using while loop.py
@@ -3,14 +3,14 @@ Movie Tickets: A movie theater charges different ticket prices depending on a pe
 If a person is under the age of 5, the ticket is free;
 if they are between 5 and 10, the ticket is $25;
 and if they are over age 10, the ticket is $50 .
-Write a loop in which you ask users their age,
+Write while loop in which you ask users their age,
 and then tell them the cost of their movie ticket.
 """
 prompt = "Enter the age of the person: "
 prompt += "\nto quit enter 'quit'. "
 
 """enter a age to get the amount"""
-while True: #keep doing until the user put quit
+while True: #keep doing until the user enter quit
     age = input(prompt)
     if age == 'quit': 
         break
