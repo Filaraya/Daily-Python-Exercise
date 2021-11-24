5271f95279e124148d0d3f250f4573e2f766af14
Filmon Y Araya <40590856+Filaraya@users.noreply.github.com>
2021-11-23 22:11:09 -0800

    Update withoutSpace_HakerRank.py

--- withoutSpace_HakerRank.py
+++ withoutSpace_HakerRank.py
@@ -2,7 +2,7 @@
 Print the list of integers from 1 through n
 as a string, without spaces.
 """
-n = int(input("Enter a n value: "))
+n = int(input("Enter a n value: ")) #enter the number 
 values='' 
 for value in range(1,n+1):
     value = str(value)
