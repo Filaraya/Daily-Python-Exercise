5edc6f88a7ef1cd882e42c08d2896bffed37699d
Filmon Y Araya <40590856+Filaraya@users.noreply.github.com>
2021-11-29 22:19:48 -0800

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -3,6 +3,7 @@ Store their first name, last name, age, and the city in which they live .
 You should have keys such as first_name, last_name, age, and city .
 Print each piece of information stored in your dictionary ."""
 
+#person dictionary
 person = {
     'first_name':'David',
     'last_name':'Jhon',
