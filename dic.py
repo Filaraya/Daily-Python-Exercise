43bcacedd51da8e875adbfcab4497f8560a64b96
Filmon Y. Araya <40590856+Filaraya@users.noreply.github.com>
2022-04-12 00:03:47 -0700

    Update dic.py

--- dic.py
+++ dic.py
@@ -1,1 +1,26 @@
-hityfdt
+# get vs [] for retrieving elements
+my_dict = {'name': 'Jack', 'age': 26}
+
+# Output: Jack
+print(my_dict['name'])
+
+# Output: 26
+print(my_dict.get('age'))
+
+# Trying to access keys which doesn't exist throws error
+# Output None
+print(my_dict.get('address'))
+
+# KeyError
+print(my_dict['address'])
+
+"""
+output
+Jack
+26
+None
+Traceback (most recent call last):
+  File "<string>", line 15, in <module>
+    print(my_dict['address'])
+KeyError: 'address'
+"""
