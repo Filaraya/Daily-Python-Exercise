32bb3079e42e4535e273bff292f92e9333b7f388
Filmon Y. Araya <40590856+Filaraya@users.noreply.github.com>
2022-08-10 23:26:25 -0700

    Update Colander.py

--- Colander.py
+++ Colander.py
@@ -1,6 +1,6 @@
 #Write a Python program to print the calendar of a given month and year.
 
-import calendar
+import calendar #import calendar module 
 y = int(input("Input the year : "))
 m = int(input("Input the month : "))
 print(calendar.month(y, m))
